2dbc0e321c176e255964dcef1bf0bdb21b2327fc
ruknez <ruknez94@mail.ru>
2019-05-01 15:51:17 +0300

    ввел передачу по ссылке

--- 7/test.cpp
+++ 7/test.cpp
@@ -2,37 +2,34 @@
 #include <iostream>
 #include <thread>
 
-bool dataReady= false;                    
-
-std::mutex mutex_;
-std::condition_variable condVar1;
-std::condition_variable condVar2;
+bool dataReady = false;                    
 
 const size_t COUNTLIMIT = 500000;
 
-void ping(){                                          
+std::condition_variable condVar1;
+std::condition_variable condVar2;
+
+
+
+void ping(std::mutex& m){                                          
 
  for (size_t i = 0; i < COUNTLIMIT; i++) {              
-    std::unique_lock<std::mutex> lock(mutex_);
+    std::unique_lock<std::mutex> lock(m);
     condVar1.wait(lock, []{return dataReady == false;});
-   // condVar1.wait (lck);
-    dataReady= true;                          
-    //std::cout << "ping" << std::endl;
     printf ("ping\n");
+    dataReady = true;                          
     condVar2.notify_one();                              
 
   }
 }
 
-void pong(){                                           
+void pong(std::mutex& m){                                           
 
   for (size_t i = 0; i < COUNTLIMIT; i++) {                            
-    std::unique_lock<std::mutex> lck(mutex_);
+    std::unique_lock<std::mutex> lck(m);
     condVar2.wait(lck, []{return dataReady == true;});
-    //condVar2.wait (lck);
-    dataReady= false;
-    //std::cout << "pong" << std::endl;
     printf ("pong\n");
+    dataReady = false;
     condVar1.notify_one();
   }
 
@@ -40,10 +37,10 @@ void pong(){
 
 int main(){
 
-  //std::cout << std::boolalpha << std::endl;
+  std::mutex mtx;
 
-  std::thread t1(ping);
-  std::thread t2(pong);
+  std::thread t1(ping,std::ref(mtx));
+  std::thread t2(pong,std::ref(mtx));
 
   t1.join();
   t2.join();
